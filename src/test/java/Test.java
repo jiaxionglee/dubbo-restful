527a6448ca9d889686d7b81110dd7fe7209f9569
jiaxiong <jiaxionglee@yeah.net>
2020-07-06 13:01:47 +0800

    up

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -13,36 +13,32 @@ import sun.nio.cs.CharsetMapping;
  */
 public class Test {
 
-    public int maxProfit(int[] prices) {
-        if (prices.length < 2) {
-            return 0;
+    public String countAndSay(int n) {
+        StringBuilder s = new StringBuilder();
+        int p1 = 0;
+        int cur = 1;
+        if ( n == 1 ) {
+            return "1";
         }
-        int minPrice = prices[0];
-        int profit = 0;
-        for (int i = 1; i < prices.length; i++) {
-            if (prices[i] < minPrice) {
-                minPrice = prices[i];
-            } else if (prices[i] - minPrice > profit) {
-                profit = prices[i] - minPrice;
+        String str = countAndSay(n - 1);
+        for ( cur = 1; cur < str.length(); cur++ ) {
+            if ( str.charAt(p1) != str.charAt(cur) ) {// 如果碰到当前字符与前面紧邻的字符不等则更新此次结果
+                int count = cur - p1;
+                s.append(count).append(str.charAt(p1));
+                p1 = cur;
             }
         }
-        return profit;
+        if ( p1 != cur ){// 防止最后一段数相等，如果不等说明p1到cur-1这段字符串是相等的
+            int count = cur - p1;
+            s.append(count).append(str.charAt(p1));
+        }
+        return s.toString();
     }
 
     public static void main(String[] args) {
         Test test = new Test();
-        int[] nums = {7, 6, 4, 3, 1};
-        String a = "1010";
-        String b = "1011";
-        System.out.println(test.maxProfit(nums));
-//        System.out.println(9^4^2);
-
-//        float sum;
-//        int i;
-//        sum=0;
-//        for ( i = 0; i < 100; i++) {
-//            sum +=0.1;
-//        }
-//        System.out.println(sum);
+        int[] nums = {1, 3, 5, 6};
+        String haystack = "race a car";
+        System.out.println(test.countAndSay(30));
     }
 }
