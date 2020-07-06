6e9d17c665842bdcbe64673e2287e1b594f60aee
jiaxiong <jiaxionglee@yeah.net>
2020-07-06 13:00:26 +0800

    zkcontroller add validated

--- src/main/java/com/ljx/dubbo/restful/controller/ZkController.java
+++ src/main/java/com/ljx/dubbo/restful/controller/ZkController.java
@@ -2,6 +2,8 @@ package com.ljx.dubbo.restful.controller;
 
 import com.ljx.dubbo.restful.util.ZkUtil;
 import io.swagger.annotations.Api;
+import javax.validation.constraints.NotBlank;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,10 +16,12 @@ import org.springframework.web.bind.annotation.RestController;
 @Api(tags = "zookeeper 相关接口")
 @RestController
 @RequestMapping("/zk")
+@Validated
 public class ZkController {
 
     @GetMapping(value = "/getDubboAddress")
-    public Object getData(@RequestParam String zk, @RequestParam String path) {
+    public Object getData(@RequestParam @NotBlank(message = "zk地址不能为空") String zk,
+            @RequestParam @NotBlank(message = "dubbo地址不能为空") String path) {
         return ZkUtil.getDubboData(zk, path);
     }
 
--- src/main/java/com/ljx/dubbo/restful/handler/GlobalExceptionHandler.java
+++ src/main/java/com/ljx/dubbo/restful/handler/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package com.ljx.dubbo.restful.handler;
 import com.ljx.dubbo.restful.exception.ApiException;
 import com.ljx.dubbo.restful.exception.ResultCode;
 import com.ljx.dubbo.restful.exception.ResultVO;
+import javax.validation.ConstraintViolationException;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -13,7 +14,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
  * @date 2020/4/19 5:08 下午
  */
 @RestControllerAdvice
-public class ControllerExceptionHandler {
+public class GlobalExceptionHandler {
 
     @ExceptionHandler(ApiException.class)
     public ResultVO<String> apiExceptionHandler(ApiException e) {
@@ -21,10 +22,15 @@ public class ControllerExceptionHandler {
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public String methodArgumentNotValidExceptionHandler(MethodArgumentNotValidException e) {
+    public ResultVO<String> methodArgumentNotValidExceptionHandler(MethodArgumentNotValidException e) {
         // 从异常对象中拿到ObjectError对象
         ObjectError objectError = e.getBindingResult().getAllErrors().get(0);
         // 然后提取错误提示信息进行返回
-        return objectError.getDefaultMessage();
+        return new ResultVO<>(ResultCode.VALIDATE_FAILED, objectError.getDefaultMessage());
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResultVO<String> constraintViolationExceptionHandler(ConstraintViolationException e) {
+        return new ResultVO<>(ResultCode.VALIDATE_FAILED, e.getLocalizedMessage());
     }
 }
